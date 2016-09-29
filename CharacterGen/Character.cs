f77343def76debeafbdf3724429cedc45ffff0d0
thegrandnizmo <thegrandnizmo@users.noreply.github.com>
2016-09-28 22:57:29 -0400

    Creating Character class containing method to update stats and store stats in arrays.

---
+++ CharacterGen/Character.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterGen
+{
+    public class Character
+    {
+        private double[] _stats;
+
+        private double _str;
+        private double _dex;
+        private double _con;
+        private double _int;
+        private double _wis;
+        private double _cha;
+
+        //List used to be converted to array
+        private List<double> mods;
+
+        //Converted from mods list
+        private double[] _mods;
+
+        public void UpdateStats(double[] stats)
+        {
+            _stats = stats;
+            _str = stats[0];
+            _dex = stats[1];
+            _con = stats[2];
+            _int = stats[3];
+            _wis = stats[4];
+            _cha = stats[5];
+            _mods = new double[6];
+            mods = new List<double>();
+
+            //Calculates ability modifiers
+            foreach (double item in stats)
+            {
+                mods.Add(Math.Floor((item - 10) / 2));
+            }
+            //Converts list to array
+            _mods = mods.ToArray();
+        }
+
+        public double StrMod
+        {
+            get
+            {
+                return _mods[0];
+            }
+            set
+            {
+
+            }
+        }
+    }
+}
---
+++ CharacterGen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new character
+            Character Char1 = new Character();
+
+            //Create character stats array
+            double[] Char1Stats = new double[6];
+
+            Console.WriteLine("Please entera character name");
+            String CharacterName = Console.ReadLine();
+
+            Console.WriteLine("Strength:");
+            Char1Stats[0] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dexterity:");
+            Char1Stats[1] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Constitution:");
+            Char1Stats[2] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Intelligence:");
+            Char1Stats[3] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Wisdom:");
+            Char1Stats[4] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Charisma:");
+            Char1Stats[5] = double.Parse(Console.ReadLine());
+
+            //Pass character stats array to Character class for storage
+            Char1.UpdateStats(Char1Stats);
+
+            Console.WriteLine(Char1.StrMod);
+        }
+    }
+}
