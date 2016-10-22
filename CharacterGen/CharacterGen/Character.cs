734337656a04b24c1127b44217e5ad3f4c2675bc
thegrandnizmo <thegrandnizmo@users.noreply.github.com>
2016-10-22 12:26:29 -0400

    Implementing skills. Changing Character._stats to int from double.

--- CharacterGen/CharacterGen/Character.cs
+++ CharacterGen/CharacterGen/Character.cs
@@ -8,7 +8,9 @@ namespace CharacterGen
 {
     public class Character
     {
-        private double[] _stats; //Holds character stats
+        private string _name;
+
+        private int[] _stats; //Holds character stats
 
         private double _strength;           //Holds individual stats pulled from _stats array.
         private double _dexterity;          //Not necessary, but makes code more readable.
@@ -17,9 +19,28 @@ namespace CharacterGen
         private double _wisdom;
         private double _charisma;
 
-        private List<double> _abilityModifiers; //List contains ability modifiers. Converts to array
+        private List<double> _abilityModifiers;
 
-        public void UpdateStats(double[] stats)
+        private double _athletics; //Skills
+        private double _acrobatics;
+        private double _sleightOfHand;
+        private double _stealth;
+        private double _arcana;
+        private double _history;
+        private double _investigation;
+        private double _nature;
+        private double _religion;
+        private double _animalHandling;
+        private double _insight;
+        private double _medicine;
+        private double _perception;
+        private double _survival;
+        private double _deception;
+        private double _intimidation;
+        private double _performance;
+        private double _persuasion;
+
+        public void UpdateStats(int[] stats)
         {
             _stats = stats;
             _strength = stats[0];
@@ -35,13 +56,78 @@ namespace CharacterGen
             {
                 _abilityModifiers.Add(Math.Floor((item - 10) / 2)); //Calculates ability modifiers
             }
+
+            _athletics = _abilityModifiers[0]; //Set Skills
+            _acrobatics = _abilityModifiers[1];
+            _sleightOfHand = _abilityModifiers[1];
+            _stealth = _abilityModifiers[1];
+            _arcana = _abilityModifiers[3];
+            _history = _abilityModifiers[3];
+            _investigation = _abilityModifiers[3];
+            _nature = _abilityModifiers[3];
+            _religion = _abilityModifiers[3];
+            _animalHandling = _abilityModifiers[4];
+            _insight = _abilityModifiers[4];
+            _medicine = _abilityModifiers[4];
+            _perception = _abilityModifiers[4];
+            _survival = _abilityModifiers[4];
+            _deception = _abilityModifiers[5];
+            _intimidation = _abilityModifiers[5];
+            _performance = _abilityModifiers[5];
+            _persuasion = _abilityModifiers[5];
         }
 
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    bool ValidName = false;
+                    while (ValidName == false)
+                    {
+                        Console.WriteLine("Name cannot be empty. Please enter a valid name:");
+                        value = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(value))
+                        {
+                            ValidName = true;
+                        }
+                    }
+                }
+                _name = value;
+            }
+        }
+        
+        //Stats
         public double StrengthModifier { get { return _abilityModifiers[0]; } }
         public double DexterityModifier { get { return _abilityModifiers[1]; } }
         public double ConstitutionModifier { get { return _abilityModifiers[2]; } }
         public double IntelligenceModifier { get { return _abilityModifiers[3]; } }
         public double WisdomModifier { get { return _abilityModifiers[4]; } }
         public double CharismaModifier { get { return _abilityModifiers[5]; } }
+
+        //Skills
+        public double SkillAthletics { get { return _athletics; } }
+        public double SkillAcrobatics { get { return _acrobatics; } }
+        public double SkillSleightOfHand { get { return _sleightOfHand; } }
+        public double SkillStealth { get { return _stealth; } }
+        public double SkillArcana { get { return _arcana; } }
+        public double SkillHistory { get { return _history; } }
+        public double SkillInvestigation { get { return _investigation; } }
+        public double SkillNature { get { return _nature; } }
+        public double SkillReligion { get { return _religion; } }
+        public double SkillAnimalHandling { get { return _animalHandling; } }
+        public double SkillInsight { get { return _insight; } }
+        public double SkillMedicine { get { return _medicine; } }
+        public double SkillPerception { get { return _perception; } }
+        public double SkillSurvival { get { return _survival; } }
+        public double SkillDeception { get { return _deception; } }
+        public double SkillIntimidation { get { return _intimidation; } }
+        public double SkillPerformance { get { return _performance; } }
+        public double SkillPersuasion { get { return _persuasion; } }
     }
 }
--- CharacterGen/CharacterGen/Program.cs
+++ CharacterGen/CharacterGen/Program.cs
@@ -12,28 +12,29 @@ namespace CharacterGen
         {
             Character PlayerCharacter = new Character(); //Instantiate new character
             
-            double[] PlayerCharacterStats = new double[6]; //Create character stats array
+            int[] PlayerCharacterStats = new int[6]; //Create character stats array
 
-            Console.WriteLine("Please entera character name");
-            String CharacterName = Console.ReadLine();
+            Console.WriteLine("Please enter a character name");
+            PlayerCharacter.Name = Console.ReadLine();
 
+            Console.WriteLine("Please enter the following stats:");
             Console.WriteLine("Strength:");
-            PlayerCharacterStats[0] = double.Parse(Console.ReadLine());
-
+            PlayerCharacterStats[0] = int.Parse(Console.ReadLine());
+            
             Console.WriteLine("Dexterity:");
-            PlayerCharacterStats[1] = double.Parse(Console.ReadLine());
+            PlayerCharacterStats[1] = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Constitution:");
-            PlayerCharacterStats[2] = double.Parse(Console.ReadLine());
+            PlayerCharacterStats[2] = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Intelligence:");
-            PlayerCharacterStats[3] = double.Parse(Console.ReadLine());
+            PlayerCharacterStats[3] = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Wisdom:");
-            PlayerCharacterStats[4] = double.Parse(Console.ReadLine());
+            PlayerCharacterStats[4] = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Charisma:");
-            PlayerCharacterStats[5] = double.Parse(Console.ReadLine());
+            PlayerCharacterStats[5] = int.Parse(Console.ReadLine());
 
             //Pass character stats array to Character class for storage
             PlayerCharacter.UpdateStats(PlayerCharacterStats);
